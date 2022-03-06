a6753a118904fa64cf625054312a922ac58b33a1
paul6ix <okporp@gmail.com>
2022-03-06 17:40:31 +0100

    Added Models to the API

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=256, default="Please provide Movie title", blank=False, null=False)
+    description = models.TextField(blank=True, null=True, max_length=1000)
+    release_date = models.DateField
+
+
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    class Meta:
+        unique_together = (('user', 'movie'),)
+        index_together = (('user', 'movie'),)
