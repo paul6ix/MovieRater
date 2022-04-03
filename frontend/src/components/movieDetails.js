ef59dec0adc5c2b4b65a3aafb7efb3bf2b2a9a48
paul6ix <okporp@gmail.com>
2022-04-03 01:04:36 +0100

    - Added ratings to the movies

--- frontend/src/components/movieDetails.js
+++ frontend/src/components/movieDetails.js
@@ -1,14 +1,24 @@
 import React, {Component} from "react";
 
+let FA = require('react-fontawesome');
+
 
 class MovieDetails extends Component {
+
     render() {
+        const movieDetail = this.props.movie
         return (
             <React.Fragment>
-                {this.props.movie ?
-                   ( <div>
-                    <h3>{this.props.movie.title}</h3><p>{this.props.movie.description}</p>
-                    </div> ) : null}
+                {movieDetail ?
+                    (<div>
+                        <h3>{movieDetail.title}</h3>
+                        <FA name='star' className={movieDetail.avg_ratings > 0 ? 'stared' : ''}/>
+                        <FA name='star' className={movieDetail.avg_ratings > 1 ? 'stared' : ''}/>
+                        <FA name='star' className={movieDetail.avg_ratings > 2 ? 'stared' : ''}/>
+                        <FA name='star' className={movieDetail.avg_ratings > 3 ? 'stared' : ''}/>
+                        ({movieDetail.get_ratings})
+                        <p>{movieDetail.description}</p>
+                    </div>) : null}
             </React.Fragment>
         )
     }
